7207c02e3db9593f9cea2bf08bf7fa82480e6b42
Florian Grandjean <florian.pdf@gmail.com>
2018-01-25 15:04:25 +0100

    Mise en place ajax autocomplete + send form
    1. Mise en place des requetes dans web/assets/js/main.js
    2. Mise en place des méthodes dans DeafultController, indexAction
    3. Ajout de style pour l'autocomplete dans web/assets/css/style.css
    4. Création d'un template à injecter dans le dom en fonction de la
    recherche (Ressources/views/default/task_result_search.html.twig)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,36 +3,108 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Task;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     *
+     * @Method("GET", "POST")
      */
     public function indexAction(Request $request)
     {
-        $form = $this->createFormBuilder()
-            ->add('search', TextType::class)
+        // CRéation d'un formulaire de recherche non lié à une entité, ce formulaire possedera l'id task_search
+        $form = $this->createFormBuilder(null, ['attr' => ['id' => 'task_search']])
+            ->add('search', TextType::class, array(
+                'attr' => array(
+                    'data-url' => $this->generateUrl('autocomplete_task'),
+                    'autocomplete' => 'off'
+                )
+            ))
             ->add('submit', SubmitType::class)
             ->getForm();
 
+        // Hydratation du form
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()){
-            $data = $form->getData();
+        // Si requête Ajax
+        if ($request->isXmlHttpRequest()){
 
+            // Récupération de la saisi du formulaire envoyé via ajax
+            $title = $form->getData()['search'];
+
+            // Récupération de la base de donnée
             $em = $this->getDoctrine()->getManager();
-            $taskTitle = $em->getRepository(Task::class)->findAutocompleteTitle($data['search']);
-            dump($taskTitle); die();
+
+            // Récupération de la task par son id
+            $task = $em->getRepository(Task::class)->findOneByTitle($title);
+
+            // Génération d'un templateque l'on injectera en jquery dans le dom
+            $view = $this->renderView('task/task_result_search.html.twig', array(
+                'task' => $task
+            ));
+
+            // Renvoie du template
+            return new Response($view);
+        }
+
+        // Si pas de requête ajax, on a un process de gestion de formulaire traditionnel
+        else{
+            if ($form->isSubmitted()){
+                $title = $form->getData()['search'];
+
+                $em = $this->getDoctrine()->getManager();
+                $task = $em->getRepository(Task::class)->findOneByTitle($title);
+
+                return $this->render('task/show.html.twig', array(
+                    'task' => $task
+                ));
+            }
         }
 
         return $this->render('default/index.html.twig', array(
             'form' => $form->createView()
         ));
+    }
+
+    /**
+     * Get all task title by pattern with ajax request
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse|\Symfony\Component\HttpFoundation\RedirectResponse
+     *
+     * @Route("/autocompletetask", name="autocomplete_task")
+     */
+    public function autoCompleteTaskAction(Request $request){
+
+        // Si on est dans une requête ajax
+        if ($request->isXmlHttpRequest()){
+
+            // Récupération du pattern saisi dans le formulaire
+            $data = $request->get('term');
+
+            // Récupération de la base de donnée
+            $em = $this->getDoctrine()->getManager();
+
+            // Réquete en base permettant de récupérer tous les titres qui possèdent le pattern
+            $taskTitles = $em->getRepository(Task::class)->findAutocompleteTitle($data);
+
+            // Retourne une réponse json avec un header adéquat
+            return new JsonResponse(json_encode($taskTitles));
+        }
+        else{
+            // Si la requête n'est pas une requête ajax, on générère une erreur
+            throw new HttpException(500, 'Not an ajax call');
+        }
     }
 }
--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,13 +1,113 @@
+// Chargement du script une fois que le document est prêt
 $(document).ready(function() {
-    Materialize.updateTextFields();
-    $('textarea').trigger('autoresize');
-    $('select').material_select();
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15, // Creates a dropdown of 15 years to control year,
-        today: 'Today',
-        clear: 'Clear',
-        close: 'Ok',
-        closeOnSelect: false // Close upon selecting a date,
-    });
+    autocompleteCustom();
+    sendForm();
 });
+
+function sendForm(){
+    // Récupération de l'évenement submit du formulaire possédant l'id task_search
+    $('#task_search').submit(function(event){
+
+        // On arrête le comportement habituel du formulaire
+        event.preventDefault();
+
+        // Appel de ajax
+        $.ajax({
+
+            // Définition de l'url de destination de la requete, cette dernière correspond à l'action définit dans le formulaire
+            url: $(this).attr('action'),
+
+            // Définition de la méthode de la requete, cette dernière correspond à la méthode défini dans le form
+            type: $(this).attr('method'),
+
+            // Formattage du formulaire
+            data: new FormData($(this)[0]),
+
+            processData: false
+
+            // Traitement du retour de la requête
+        }).then(function(response){
+
+            // Injection du template reçu dans le dom
+            $('#search_result').html(response);
+
+            // Cas d'erreur
+        }).catch(function(error){
+            console.log(error);
+        })
+    })
+}
+
+function autocompleteCustom(){
+    // Récupération de l'évènement keyup (puch sur une touche du clavier)
+    $('#form_search').keyup(function(event){
+
+        // Si la touche pressé est un caractère alphanumeric
+        if (event.which <= 90 && event.which >= 48)
+        {
+            // Récupération du pattern saisi dans le formulaire
+            var pattern = $(this).val();
+
+            // Récupération de l'url de destination de la requête
+            var url = $(this).data('url');
+
+            // Si plus de 2 caractères ont été saisi
+            if (pattern.length >= 2){
+
+                // Appel ajax
+                $.ajax({
+
+                    // Définition du type de la méthode
+                    type: 'POST',
+
+                    // Définition de l'url de destination de la méthode
+                    url: url,
+
+                    // Type de donnée que l'on va envoyer
+                    dataType: 'json',
+
+                    // Donnée à envoyer
+                    data: {'term' : pattern},
+
+                    // Empeche d'emmettre plus d'une requête par 2 secondes
+                    timeout: 2000
+
+                    // Un fois la requête effectué, récupération de la réponse
+                }).then(function(response){
+
+                    // Transformation de la réponse json(chaine de caractères) en objet json
+                    var response = JSON.parse(response);
+
+                    // Définition d'une variable qui contiendra la liste des propositions
+                    var contentUl = '';
+
+                    // Boucle sur notre tableau de réponse
+                    for(i = 0; i < response.length; i++){
+
+                        // Création des li pour chaque élément retourné + concatenation avec le contenu du ul
+                        contentUl += "<li>" + response[i].title + "</li>";
+                    }
+
+                    // Injection du contenu précédemment créé dans le dom
+                    $('#result_autocomplete').html(contentUl);
+
+                    // Récupération de l'évènement click sur une li (proposition)
+                    $('#result_autocomplete li').click(function(){
+
+                        // Récupération du contenu de l'élément sur lequel on a cliqué
+                        var selectValue = $(this).text();
+
+                        // Injection dans l'input du formulaire de l'élément sur lequel on a cliqué
+                        $('#form_search').val(selectValue);
+
+                        // Remise à zéro de la liste
+                        $('#result_autocomplete').html('');
+                    })
+                    // En cas d'erreur
+                }).catch(function(error){
+                    console.log(error);
+                })
+            }
+        }
+    })
+}
